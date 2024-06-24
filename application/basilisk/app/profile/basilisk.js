41257acb582965cd22ae3fdb0459cfb1c72972e7
K4sum1 <k4sum1@eclipse.cx>
2024-06-24 12:37:05 -0700

    Enable one off searches by default

--- application/basilisk/app/profile/basilisk.js
+++ application/basilisk/app/profile/basilisk.js
@@ -289,11 +289,7 @@ pref("browser.urlbar.suggest.history.onlyTyped",    false);
 pref("browser.urlbar.formatting.enabled", true);
 pref("browser.urlbar.trimURLs", true);
 
-#if defined(NIGHTLY_BUILD)
 pref("browser.urlbar.oneOffSearches", true);
-#else
-pref("browser.urlbar.oneOffSearches", false);
-#endif
 
 pref("browser.altClickSave", false);
 
