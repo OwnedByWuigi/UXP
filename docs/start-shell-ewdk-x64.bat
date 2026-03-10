29f4c429ec1f102b1a6aaddbc75f447fad2a8572
OwnedByWuigi <ownedbywuigi@gmail.com>
2026-03-10 19:58:14 +0000

    Add a compiling guide

---
+++ docs/start-shell-ewdk-x64.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+SET MOZ_MSVCBITS=64
+SET MOZ_MSVCVERSION=14
+SET MOZ_MSVCYEAR=2017
+SET VCDIR=C:\DEV\MSVC14\
+SET TOOLCHAIN=64-bit
+SET MOZILLABUILD=%~dp0
+SET MOZ_TOOLS=%MOZILLABUILD%moztools
+SET VSINSTALLDIR=C:\DEV\MSVC14
+SET VCINSTALLDIR=C:\DEV\MSVC14
+SET FrameworkDir32=C:\Windows\Microsoft.NET\Framework\
+SET FrameworkVersion32=v4.0.30319
+SET Framework40Version=v4.0.30319
+SET UCRTVersion=10.0.19041.0
+SET WindowsSDKLibVersion=10.0.19041.0\
+SET INCLUDE=C:\Dev\MSVC14\include;C:\Dev\MSVC14\atlmfc\include;C:\Dev\Windows Kits\10\include\shared;C:\Dev\Windows Kits\10\include\10.0.19041.0\um;C:\Dev\Windows Kits\10\include\10.0.19041.0\winrt;C:\Dev\Windows Kits\10\include\10.0.19041.0\ucrt;
+SET LIB=C:\Dev\MSVC14\lib\x64;C:\Dev\MSVC14\atlmfc\lib\x64;C:\Dev\Windows Kits\10\lib\10.0.19041.0\um\x64;C:\Dev\Windows Kits\10\lib\10.0.19041.0\ucrt\x64;
+SET LIBPATH=C:\Dev\MSVC14\lib\x64;C:\Dev\MSVC14\atlmfc\lib\x64;C:\Dev\Windows Kits\10\lib\10.0.19041.0\ucrt\x64;
+SET PATH=C:\Dev\MSVC14\bin\Hostx64\x64;C:\Dev\MSVC14\bin\Hostx64\x64;C:\Dev\Windows Kits\10\bin\10.0.19041.0\x64;%PATH%
+
+SET SDKDIR=C:\Dev\Windows Kits\10\
+SET WINDOWSSDKDIR=C:\Dev\Windows Kits\10\
+set  "UCRTContentRoot=%WDKContentRoot%"
+set  "UniversalCRTSdkDir=%WDKContentRoot%"
+START %MOZILLABUILD%msys\bin\mintty -e %MOZILLABUILD%msys\bin\console %MOZILLABUILD%msys\bin\bash --login
